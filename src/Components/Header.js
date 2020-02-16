17b4955af1e724034bfb004bc1f3ff1a29823097
pranav2308 <pranav08@terpmail.umd.edu>
2020-02-15 19:43:24 -0500

    Changed Header and about me section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,6 +9,7 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
+        console.log(`${network.name} ${network.url} ${network.className}`);
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
